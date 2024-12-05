a0a3d2d6318524b07ac46210070ba895890ffcb5
Hira khan <hira@logicchaos.com>
2024-12-06 00:48:28 +0500

    added: minor fixes completed drawer

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,10 +1,37 @@
 "use client";
 import { ASSETS } from "@/public/path";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { UserButton, useUser } from "@clerk/nextjs";
+import { RiCloseFill } from "react-icons/ri";
+import { GiHamburgerMenu } from "react-icons/gi";
 export const Nav = () => {
+  const [drawerState, setDrawerState] = useState(false);
+  const drawerRef = useRef<any>(null);
   const { user } = useUser(); // Get user info from Clerk
+  const onToggleDrawer = useCallback(() => {
+    console.log('called')
+    setDrawerState((prevState) => !prevState);
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (drawerRef.current && !drawerRef.current.contains(event.target)) {
+        setDrawerState(false);
+      }
+    };
+
+    if (drawerState) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [drawerState]);
+
   return (
     <div className="w-full flex items-center justify-center h-[10vh] bg-black bg-opacity-80 text-white">
       <div className="flex items-center justify-between w-full max-w-[1200px] px-10">
@@ -47,7 +74,40 @@ export const Nav = () => {
             </li>
           )}
         </ul>
+        <div className="md:hidden">
+          <GiHamburgerMenu
+            className="cursor-pointer text-sm"
+            onClick={onToggleDrawer}
+          />
+        </div>
       </div>
+      {drawerState && (
+        <div
+          ref={drawerRef}
+          className="min-h-screen bg-white duration-300 fixed top-0 left-0 w-full z-[1000]  p-5 overflow-y-auto"
+        >
+          <div className="flex flex-col items-center h-full w-full min-h-screen text-black ">
+            <div className="flex items-center justify-end w-full">
+              <RiCloseFill
+                onClick={onToggleDrawer}
+                className="text-red-500 text-3xl cursor-pointer"
+              />
+            </div>
+            <div className="space-y-4 text-black flex flex-col text-sm">
+              <Link href="/write-a-review" >Write a review</Link>
+              <Link href="/write-a-review">Reviews</Link>
+              <Link href="/categories">Categories</Link>
+              <Link href="/categories">About us</Link>
+              <Link href="/blog">Blog</Link>
+              {!user && <Link href={user ? "/" : "/auth/sign-in"}>Log in</Link>}
+              {user && <UserButton />}
+              {!user && (
+              <Link href="/business">For Businesses</Link>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
